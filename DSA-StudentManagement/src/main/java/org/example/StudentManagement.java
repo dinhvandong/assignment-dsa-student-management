65aa04dbce8d9120fb7bc378f62a25a9e04847ea
dinhvandong <dongvan.ict3@gmail.com>
2024-10-16 10:14:28 +0700

    xx
    xxx

---
+++ DSA-StudentManagement/src/main/java/org/example/StudentManagement.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class StudentManagement {
+    StudentStack studentStack;
+    public  StudentManagement(){
+        studentStack = new StudentStack();
+    }
+    public void addStudent(Student student)
+    {
+    }
+
+}
